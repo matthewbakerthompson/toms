ea3fb141f4c3acabe97e5503f0bb5dfbf42cc710
matthewbakerthompson <matthewbakerthompson@gmail.com>
2021-07-13 23:12:33 -0400

    initial commit

---
+++ toms.py
@@ -0,0 +1,14 @@
+# toms code
+
+
+
+# Creating a List
+import random
+List1 = ["fightin a bean man", "smokeing", "horsin around", "getting out of tha town", "i was running"]
+List2 = ["tom", "meg", "loontenant dan", "wilson", "the big keyboard from big"]
+item1 =  random.choice(List1)
+item2 = random.choice(List2)
+print("Random tom hank movie Plot Generator:")
+print(item1)
+print(" with ")
+print(item2)
